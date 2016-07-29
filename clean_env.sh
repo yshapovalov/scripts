c8e8d0b34abebfb921c3a6620146ac082ef9bb9b
yshapovalov <yshapovalov@mirantis.com>
2016-07-29 18:40:28 +0300

    Update clean_env.sh

--- clean_env.sh
+++ clean_env.sh
@@ -10,8 +10,8 @@ done
 NETS=$(neutron net-list | grep snat | cut -d "|" -f 2)
 for net_uuid in $NETS
 do
-    neutron net-delte $net_uuid
+    neutron net-delete $net_uuid
 done
 
 PROJECT=$(openstack project list | grep demo | cut -d "|" -f 2)
-openstack project delte $PROJECT
+openstack project delete $PROJECT
